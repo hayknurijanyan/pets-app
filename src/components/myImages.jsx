edbd40c6b862b310a32de1c6dd456ce3d84f94db
Ash <apetrosyan13@mail.ru>
2020-08-13 06:09:28 -0700

    like buttons added

--- src/components/myImages.jsx
+++ src/components/myImages.jsx
@@ -5,94 +5,95 @@ import GridListTile from '@material-ui/core/GridListTile';
 import {
     catdog
 } from '../images/catdog.png';
+import { IconButton, GridListTileBar } from '@material-ui/core';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 const tileData = [{
     img: "https://coverfiles.alphacoders.com/927/92705.jpg",
     cols: 2,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://www.pethealthnetwork.com/sites/default/files/articles/dogs-cats-birds-and-bunnies-one-house-fb-167580013_0.jpg",
     cols: 1,
-    title: "Image"
-
+    title: "Image name"
 },
 {
     img: "https://coverfiles.alphacoders.com/927/92705.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://www.pethealthnetwork.com/sites/default/files/articles/dogs-cats-birds-and-bunnies-one-house-fb-167580013_0.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://coverfiles.alphacoders.com/927/92705.jpg",
     cols: 2,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://www.pethealthnetwork.com/sites/default/files/articles/dogs-cats-birds-and-bunnies-one-house-fb-167580013_0.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://coverfiles.alphacoders.com/927/92705.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://www.pethealthnetwork.com/sites/default/files/articles/dogs-cats-birds-and-bunnies-one-house-fb-167580013_0.jpg",
     cols: 2,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://coverfiles.alphacoders.com/927/92705.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 },
 {
     img: "https://www.pethealthnetwork.com/sites/default/files/articles/dogs-cats-birds-and-bunnies-one-house-fb-167580013_0.jpg",
     cols: 1,
-    title: "Image"
+    title: "Image name"
 
 }]
 const useStyles = makeStyles((theme) => ({
@@ -108,6 +109,9 @@ const useStyles = makeStyles((theme) => ({
   gridList: {
     width: '80%',
   },
+  title: {
+    color: theme.palette.primary.light,
+  },
 }));
 
 export default function ImageGridList() {
@@ -118,7 +122,19 @@ export default function ImageGridList() {
       <GridList cellHeight={160} className={classes.gridList} cols={3}>
         {tileData.map((tile) => (
           <GridListTile key={tile.img} cols={tile.cols}>
-            <img src={tile.img} alt={tile.title} />
+            <img src={tile.img} alt={tile.title} onClick={()=>console.log("asd")}/>
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <FavoriteBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
           </GridListTile>
         ))}
       </GridList>
