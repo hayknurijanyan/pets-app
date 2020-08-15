5455c7bbbf663e1cd934ada0aedc40ea91d06a0b
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-08-15 16:41:58 +0400

    added profile

---
+++ src/components/profile/avatar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+export function ImageAvatarSmall() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Avatar
+        component={Link}
+        to="/profile"
+        alt="Albert Einstein"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRp2JhPKPKlf3S80BQXaCDuK0bsOgVgK8mc5g&usqp=CAU"
+        className={classes.small}
+      />
+    </div>
+  );
+}
+export default function ImageAvatar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Avatar
+        component={Link}
+        to="/profile"
+        alt="Albert Einstein"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRp2JhPKPKlf3S80BQXaCDuK0bsOgVgK8mc5g&usqp=CAU"
+        className={classes.large}
+      />
+    </div>
+  );
+}
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Toolbar, Card } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Account from "./account";
+import About from "./about";
+import { useSelector } from "react-redux";
+import firebase from "firebase";
+import ImageGridList from "./myImages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Friends from "../friends";
+import AllPets from "../allpets";
+import FriendsCard from "./friendscard";
+import PetsCard from "./petscard";
+let log = console.log;
+const useStyles = makeStyles((theme) => ({
+  card: {
+    margin: 10,
+    width: "45 rem",
+    display: "none",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+const Profile = () => {
+  const classes = useStyles();
+  const user = firebase.auth().currentUser;
+
+  if (user) {
+    log("currnetuser", user);
+  } else {
+    log("asd");
+  }
+
+  const isUser = useSelector((state) => state);
+  log("user redux", isUser);
+  log("state", isUser);
+
+  return (
+    <Router>
+      <div>
+        <Toolbar />
+        <Account />
+        <Card className={classes.card}></Card>
+        <Switch>
+          <Route path="/profile/photos" component={ImageGridList} />
+          <Route path="/profile/about" component={About} />
+          <Route path="/profile/friends" component={FriendsCard} />
+          <Route path="/profile/pets" component={PetsCard} />
+          <Route path="/profile/" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default Profile;
