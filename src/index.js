c53fa233739febf6a2275d6cca31d0833192bb7b
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-08-19 13:58:10 +0400

    some work done with redux

--- src/index.js
+++ src/index.js
@@ -11,17 +11,34 @@ import { Provider } from "react-redux";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import autoMergeLevel1 from "redux-persist/lib/stateReconciler/autoMergeLevel1";
+import Loader from "./components/loader";
 
-export const store = createStore(
-  allReducers,
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel1,
+  whitelist: ["userReducer"], // how to merge incoming state
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
