eb754f9b5778a450ac3fe683d4f7736dfc5c5b9c
hayk <naregalfayan@Nares-MacBook-Pro.local>
2020-08-20 05:29:02 -0700

    sign up changed

--- src/components/authentication/petsSecelctFiled.jsx
+++ src/components/authentication/petsSecelctFiled.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+
+import {
+  TextField,
+  Grid,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from "@material-ui/core";
 import { db, storage } from "../../firebase";
 
 let log = console.log;
@@ -17,6 +22,20 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 5,
     marginBottom: 5,
     minWidth: 350,
+  },
+  formControlNew: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  petInput: {
+    marginTop: 5,
+    marginBottom: 4,
   },
 }));
 
@@ -43,6 +62,19 @@ export default function PetsSelectFiled(props) {
     props.onHandlePetSet(event.target.value);
   };
 
+  const handleBreed = async (event) => {
+    props.onHandleBreedSet(event.target.value);
+  };
+  const handlePetName = async (event) => {
+    props.onHandlePetName(event.target.value);
+  };
+  const handlePetAge = async (event) => {
+    props.onHandlePetAge(event.target.value);
+  };
+  const handlePetGender = async (event) => {
+    props.onHandlePetGender(event.target.value);
+  };
+
   const handleClose = () => {
     setOpen(false);
   };
@@ -52,27 +84,95 @@ export default function PetsSelectFiled(props) {
 
   return (
     <div>
-      <FormControl fullWidth className={classes.formControl}>
-        <InputLabel id="demo-controlled-open-select-label">Pet</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          value={props.pet}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          onChange={handleChange}
+      <Grid className={classes.petInput}>
+        <FormControl
+          variant="outlined"
+          fullWidth
+          className={classes.formControl}
         >
-          <MenuItem>None</MenuItem>
-          {list.map((animal, index) => {
-            return (
-              <MenuItem key={(index += "asd")} value={animal}>
-                {animal}
+          <InputLabel id="demo-simple-select-outlined-label">Pet</InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            label="Pet"
+            open={open}
+            value={props.pet}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            onChange={handleChange}
+          >
+            <MenuItem>None</MenuItem>
+            {list.map((animal, index) => {
+              return (
+                <MenuItem key={(index += "asd")} value={animal}>
+                  {animal}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            onChange={handlePetName}
+            value={props.petName}
+            placeholder="Pet Name"
+            autoComplete="pname"
+            name="Pet Name"
+            variant="outlined"
+            isRequired="true"
+            fullWidth
+            id="petname"
+            label="Pet Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            onChange={handleBreed}
+            value={props.breed}
+            variant="outlined"
+            fullWidth
+            placeholder="Breed"
+            id="breed"
+            label="Breed"
+            name="breed"
+            autoComplete="lname"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            className={classes.formControlNew}
+            onChange={handlePetAge}
+            value={props.petAge}
+            fullWidth
+            id="standard-number"
+            label="Age"
+            type="number"
+            name="age"
+            autoComplete="lname"
+            InputProps={{ inputProps: { min: 0 } }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth className={classes.formControlNew}>
+            <InputLabel id="demo-simple-select-helper-label">Gender</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={props.petGender}
+              onChange={handlePetGender}
+            >
+              <MenuItem value="">
+                <em>None</em>
               </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+              <MenuItem value={1}>Male</MenuItem>
+              <MenuItem value={2}>Female</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
     </div>
   );
 }
