f745d3d6a2d7b501bc94bb661ef43a91efc44163
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-08-23 23:20:32 +0400

    bug fixes

--- src/components/displayUsers/user.jsx
+++ src/components/displayUsers/user.jsx
@@ -67,7 +67,7 @@ function User(props) {
                             >
                               {`@${obj.pet}`}
                             </Typography>
-                            {obj.location.city ? ` -${obj.location.city}` : ""}
+                            {obj.location.city ? `  ${obj.location.city}` : ""}
                           </React.Fragment>
                         }
                       />
