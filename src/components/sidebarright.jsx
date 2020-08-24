587a6e8181d6dd72cf7f3eb64dae2b52102b9bcd
hayk <naregalfayan@Nares-MacBook-Pro.local>
2020-08-24 13:45:55 -0700

    some changes

--- src/components/sidebarright.jsx
+++ src/components/sidebarright.jsx
@@ -88,14 +88,18 @@ export default function SidebarRight() {
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary="Mail" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
       </List>
       <ChatBox />
     </Drawer>
