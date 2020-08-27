4d914b6c4c918994751a34d277ac3e0669d1baa2
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-08-27 20:56:09 +0400

    themes done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,11 @@ import userReducer from "./isUser";
 import urlReducer from "./isUrl";
 import { combineReducers } from "redux";
 import userDataReducer from "./userData";
-import themeSelectReducer from "./themeSelect";
 
 const allReducers = combineReducers({
   user: userReducer,
   isUrl: urlReducer,
   userData: userDataReducer,
-  themeSelect: themeSelectReducer,
 });
 
 export default allReducers;
