e704a98030479e68344288da06c8c79312301e2f
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-08-30 19:34:16 +0400

    forgot password done

---
+++ src/components/authentication/forgotPassword.jsx
@@ -0,0 +1,189 @@
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { isUserAction, userDataAction } from "../../actions";
+import {
+  Avatar,
+  Button,
+  Grid,
+  Typography,
+  Link,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Paper,
+  Box,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { db } from "../../firebase";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+let log = console.log;
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Charo
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage:
+      "url(https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "white",
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function ForgotPassword() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [successOpen, setSuccessOpen] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleResetSend = async () => {
+    const auth = firebase.auth();
+    auth
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        setSuccess({ message: "The Email has been sent!" });
+        checkSuccess(success);
+        setSuccessOpen(true);
+        setEmail("");
+      })
+      .catch((err) => {
+        setError(err);
+        checkError(error);
+        setOpen(true);
+      });
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+    setSuccessOpen(false);
+  };
+
+  const checkError = (err) => {
+    return (
+      <Alert onClose={handleClose} severity="error">
+        {err.message}
+      </Alert>
+    );
+  };
+  const checkSuccess = (succ) => {
+    return (
+      <Alert onClose={handleClose} severity="success">
+        {succ.message}
+      </Alert>
+    );
+  };
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        {checkError(error)}
+      </Snackbar>
+      <Snackbar
+        open={successOpen}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      >
+        {checkSuccess(success)}
+      </Snackbar>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Forgot Password?
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              value={email}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="email"
+              name="em ail"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmail}
+            />
+
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleResetSend}
+            >
+              Send request
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="signin" variant="body2">
+                  {"Back to Sign in"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ForgotPassword;
